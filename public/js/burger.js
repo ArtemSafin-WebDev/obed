7b5519a7668c962d9850b9b651093232da8f1846
Artem Safin <artemsafin.webdev@gmail.com>
2024-11-01 04:48:10 +0300

    По кнопке профиля

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const mobileProfile = document.querySelector(".page-header__mobile-profile");
+
+  if (mobileProfile) {
+    const button = mobileProfile.querySelector(
+      ".page-header__mobile-profile-btn"
+    );
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.remove("mobile-menu-open");
+      mobileProfile.classList.toggle("shown");
+    });
+
+    document.addEventListener("click", (event) => {
+      if (mobileProfile.contains(event.target)) return;
+      mobileProfile.classList.remove("shown");
+    });
+  }
   const burger = document.querySelector(".page-header__burger");
   if (!burger) return;
 
